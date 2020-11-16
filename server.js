f22ae4fed743cd868ae43cf5f88e396f16be6be1
Maxence <maxence.berger41@gmail.com>
2020-11-16 17:52:33 +0100

    Add Map

--- server.js
+++ server.js
@@ -3,48 +3,45 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express()
 
-// const apiKey = process.env.API_KEY || 'NC';
-// const apiKeyMap = process.env.API_KEY || 'NC';
+const apiKey = process.env.API_KEY || 'NC';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index', {weather: null, error: null});
+  res.render('index', {weather: null, long: null, lat: null, error: null});
 })
 
 app.post('/', function (req, res) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-  let urlMap = `https://api.mapbox.com/geocoding/v5/mapbox.places/${city}.json?access_token=${apiKeyMap}`
 
   console.log("URL: "+url)
   request(url, function (err, response, body) {
     console.log("Status "+response.statusCode);
 
     if(response.statusCode === 401){
-      res.render('index', {weather: null, error: 'Invalid API token'});
+      res.render('index', {weather: null, long: null, lat: null, error: 'Invalid API token'});
     }else if(response.statusCode === 404){
-      res.render('index', {weather: null, error: 'City not found'});
+      res.render('index', {weather: null, long: null, lat: null, error: 'City not found'});
     } else {
       let weather = JSON.parse(body)
       if(weather.main === undefined){
-        res.render('index', {weather: null, error: 'Error, please try again'});
+        res.render('index', {weather: null, long: null, lat: null, error: 'Error, please try again'});
       } else {
         let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        res.render('index', {weather: weatherText, error: null});
+
+        request(url, function (err, response, body) {
+          console.log("Status "+response.statusCode);
+          let map = JSON.parse(body)
+          let mapLong = JSON.stringify(map.coord.lon)
+          let mapLat = JSON.stringify(map.coord.lat)
+          res.render('index', {weather: weatherText, long: mapLong, lat: mapLat, error: null});
+        });
       }
     }
-  }),
-      console.log("URL_MAP: "+urlMap)
-  request(url, function (err, response, body) {
-    console.log("Status "+response.statusCode);
-    let map = JSON.parse(body)
-    console.log(JSON.stringify("LON "+map.coord.lon));
-    console.log(JSON.stringify("LAT "+map.coord.lat));
-    res.render('index', {long: map.coord.lon, error: null});
-  });
+  })
 })
 
 const PORT = process.env.PORT || 3000;
