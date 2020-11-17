9f09f7236aa94c9cb2dceadf862d2748db3eb060
Maxence <maxence.berger41@gmail.com>
2020-11-17 10:19:47 +0100

    Add features

--- server.js
+++ server.js
@@ -2,45 +2,43 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const app = express()
-const dotenv = require('dotenv');
-dotenv.config();
 
 const apiKey = process.env.API_KEY || 'NC';
-const apiKeyMap = process.env.API_KEY_MAP || 'NC';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index', {weather: null, apiKeyMap: apiKeyMap, long: null, lat: null, error: null});
+  res.render('index', {weather: null, icon: null, iconInfo: null,long: null, lat: null, error: null});
 })
 
 app.post('/', function (req, res) {
   let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 
   console.log("URL: "+url)
   request(url, function (err, response, body) {
     console.log("Status "+response.statusCode);
 
     if(response.statusCode === 401){
-      res.render('index', {weather: null, long: null, lat: null, error: 'Invalid API token'});
+      res.render('index', {weather: null, icon: null, iconInfo: null, long: null, lat: null, error: 'Invalid API token'});
     }else if(response.statusCode === 404){
-      res.render('index', {weather: null, long: null, lat: null, error: 'City not found'});
+      res.render('index', {weather: null, icon: null, iconInfo: null, long: null, lat: null, error: 'City not found'});
     } else {
       let weather = JSON.parse(body)
       if(weather.main === undefined){
-        res.render('index', {weather: null, long: null, lat: null, error: 'Error, please try again'});
+        res.render('index', {weather: null, icon: null, iconInfo: null, long: null, lat: null, error: 'Error, please try again'});
       } else {
-        let informationsData = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        let weatherText = `It's ${weather.main.temp} degrees C° in ${weather.name}!`;
 
         request(url, function (err, response, body) {
           console.log("Status "+response.statusCode);
           let map = JSON.parse(body)
           let mapLong = JSON.stringify(map.coord.lon)
           let mapLat = JSON.stringify(map.coord.lat)
-          res.render('index', {weather: informationsData, long: mapLong, lat: mapLat, error: null});
+          console.log(JSON.stringify(weather))
+          res.render('index', {weather: weatherText, icon: weather.weather[0].icon, iconInfo: weather.weather[0].description , long: mapLong, lat: mapLat, error: null});
         });
       }
     }
